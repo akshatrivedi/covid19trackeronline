bce15c1bfbdab5fc8833b8374006cfc7822a66c2
Saket <kumar.saket@capgemini.com>
2022-10-06 11:07:59 +0530

    first commit

---
+++ src/test/java/com/capgemini/coronavirustracker/Covid19projectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
